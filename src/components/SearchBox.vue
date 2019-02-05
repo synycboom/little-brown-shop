9c6d3e635203d9ce226d0bd0e3852679442739f3
synycboom <synycboom@gmail.com>
2019-02-05 16:53:16 +0700

    WIP: Implementing SearchBox and ViewType

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="searchbox">
+    <input type="text">
+    <FontAwesomeIcon icon="barcode" size="3x"/>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '../assets/scss/vars.scss';
+
+.searchbox {
+  display: flex;
+  width: 40%;
+  min-width: 230px;
+  max-width: 380px;
+  height: 2.5rem;
+
+  input {
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-top: 1px solid $primaryColor;
+    border-left: 1px solid $primaryColor;
+    border-right: 0px;
+    border-bottom: 1px solid $primaryColor;
+    border-radius: 0px;
+  }
+
+  svg {
+    border-left: 0px;
+    border-top: 1px solid $primaryColor;
+    border-right: 1px solid $primaryColor;
+    border-bottom: 1px solid $primaryColor;
+    padding-left: 5px;
+    padding-right: 5px;
+    height: 2.5rem;
+  }
+}
+</style>
---
+++ src/components/ViewType.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="view-type">
+    <FontAwesomeIcon icon="th" size="3x" class="grid-view" />
+    <FontAwesomeIcon icon="list" size="3x" class="list-view" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewType'
+};
+</script>
+
+<style lang="scss">
+@import '../assets/scss/vars.scss';
+
+.view-type {
+  display: inline-block;
+  border: 1px solid $primaryColor;
+  height: 2.5rem;
+  min-width: 100px;
+
+  .grid-view {
+    padding: 8px;
+    height: inherit;
+  }
+
+  .list-view {
+    padding: 8px;
+    height: inherit;
+  }
+}
+</style>
